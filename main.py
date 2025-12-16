7fec53db9ba8b89e6c2e0c0fd0f2e32520d44532
LancerFDa <rodrgolorenzo@gmail.com>
2025-12-16 10:56:12 +0100

    refactor(main): Cambiar nombre de los importes y la funcion find_individual_fitness

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from src.generate_solution import generate_solution
 from src.generate_first_population import generate_first_population
-from src.find_individual_fitness import find_fitness
+from src.find_individual_fitness import find_individual_fitness
 
 def main():
 
     solution = generate_solution()
     population = generate_first_population()
-    find_fitness(solution, population)
+    find_individual_fitness(solution, population)
 
 if __name__ == "__main__":
     main()
