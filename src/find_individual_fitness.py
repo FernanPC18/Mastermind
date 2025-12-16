a57e95763f4ced9dd06880af627613b2a2dcff34
LancerFDa <rodrgolorenzo@gmail.com>
2025-12-16 11:10:56 +0100

    fix(find_individual_fitness): Arreglar que no se añadía valor al fitness
    Co-authored-by: FernanPC18 <FernanPC18@users.noreply.github.com>

--- src/find_individual_fitness.py
+++ src/find_individual_fitness.py
@@ -3,6 +3,6 @@ def find_individual_fitness(solution, population):
 
     for i in population:
         for j in enumerate(i):
-            if j in solution:
+            if j in enumerate(solution):
                 fitness += [1]
     return fitness
