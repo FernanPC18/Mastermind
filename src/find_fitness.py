3d557166a3a6a920dad7cbae8f84fa933ed57ae7
LancerFDa <rodrgolorenzo@gmail.com>
2025-12-16 10:44:09 +0100

    refactor(find_individual_fitness): Cambiar el nombre del modulo
    Crear el nombre del módulo find_individual_fitness  y refactorizarlo

--- src/find_fitness.py
+++
@@ -1,7 +0,0 @@
-def find_fitness(solution, population):
-    fitness = 0
-    
-    for i in enumerate(population):
-        if enumerate(population) == enumerate(solution) or population in solution:
-            fitness += 1    
-    return fitness
---
+++ src/find_individual_fitness.py
@@ -0,0 +1,8 @@
+def find_individual_fitness(solution, population):
+    fitness = []
+
+    for i in population:
+        for j in enumerate(i):
+            if j in solution:
+                fitness += [1]
+    return fitness
