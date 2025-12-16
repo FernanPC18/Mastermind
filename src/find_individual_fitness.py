95d5c44cbd95f1a97b8cb10950a8255dac698a6a
LancerFDa <rodrgolorenzo@gmail.com>
2025-12-16 23:46:21 +0100

    fix(find_individual_fitness): Poder guardar el fitness de cada individuo
    Se puede guardar el fitness de cada individuo desde un disccionario, faltan retoques

--- src/find_individual_fitness.py
+++ src/find_individual_fitness.py
@@ -2,11 +2,13 @@ def find_individual_fitness(solution, population):
     fitness = {}
     puntuation = 0
 
-    for i in population:
+    for posicion, i in enumerate(population):
         for j in enumerate(i):
             j_position = j[0]
-            if j[1] == solution[j_position]:
+            if j[1] == solution[j_position] or (j in solution):
                 puntuation += 1
             else:
                 puntuation -= 1
+        fitness[posicion + 1] = puntuation
+        
     return fitness
