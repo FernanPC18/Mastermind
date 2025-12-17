36bc9d6281f3f94a9a9b620286110cff0255a2e9
LancerFDa <rodrgolorenzo@gmail.com>
2025-12-17 11:48:48 +0100

    fix(find_individual_fitness): Cambiar nombres de genes y alelos de los individuos

--- src/find_individual_fitness.py
+++ src/find_individual_fitness.py
@@ -3,9 +3,9 @@ def find_individual_fitness(solution, population):
 
     for individual_number, individual in enumerate(population):
         puntuation = 0
-        for gene_individual in enumerate(individual):
-            gene_population = gene_individual[0]
-            if gene_individual[1] == solution[gene_population] or (gene_individual in solution):
+        for gene_allele_individual in enumerate(individual):
+            gene_individual = gene_allele_individual[0]
+            if gene_allele_individual[1] == solution[gene_individual] or (gene_allele_individual in solution):
                 puntuation += 1
             else:
                 puntuation -= 1
