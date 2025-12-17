afb3168137b7b67a07400ff4bbb0222396329835
LancerFDa <rodrgolorenzo@gmail.com>
2025-12-17 12:09:20 +0100

    fix(find_individual_fitness): Solucionar problema de duplicado de fitness
    El fitness no se pasa de 4 ni de -4

--- src/find_individual_fitness.py
+++ src/find_individual_fitness.py
@@ -5,7 +5,9 @@ def find_individual_fitness(solution, population):
         puntuation = 0
         for gene_allele_individual in enumerate(individual):
             gene_individual = gene_allele_individual[0]
-            if gene_allele_individual[1] == solution[gene_individual] or (gene_allele_individual in solution):
+            if gene_allele_individual[1] == solution[gene_individual]:
+                puntuation += 1
+            elif gene_allele_individual[1] in set(solution):
                 puntuation += 1
             else:
                 puntuation -= 1
