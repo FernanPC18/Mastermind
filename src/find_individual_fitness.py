4bc9b060baf15432c483f5624e8d9196e53eabbc
Fernan <231477850+FernanPC18@users.noreply.github.com>
2025-12-30 17:05:17 +0100

    fix(find_individual_fitness): Arreglar puntuación duplicada del fitness
    Co-authored-by: LancerFDa <LancerFDa@users.noreply.github.com>

--- src/find_individual_fitness.py
+++ src/find_individual_fitness.py
@@ -6,17 +6,26 @@ def find_individual_fitness(solution, population):
         # fitness_score es la puntuación que tendrá cada individuo
         fitness_score = 4
         individual_copy = individual[:]
+        solution_copy = solution[:]
 
         # * gene_allele_individual es el indice y uno de los elementos de individual
+
         for gene_individual, allele_individual in enumerate(individual_copy):
-            individual_copy[gene_individual] = None
 
             if allele_individual == solution[gene_individual]:
                 fitness_score += 1
+                individual_copy[gene_individual] = None
+                solution_copy[gene_individual] = None
+
             elif allele_individual in solution:
                 fitness_score += 1
+                individual_copy[gene_individual] = None
+                solution_copy[gene_individual] = None
+
             else:
                 fitness_score -= 1
+                individual_copy[gene_individual] = None
+                solution_copy[gene_individual] = None
 
         fitness[individual_number] = fitness_score
 
