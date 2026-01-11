a49ecca53848437ab8e3a37ef8c0d64022dc4efb
LancerFDa <rodrgolorenzo@gmail.com>
2026-01-11 14:45:30 +0100

    feat(check_solution): al llegar a la solución se frena el programa
    Co-authored-by: FernanPC18 <FernanPC18@users.noreply.github.com>

--- src/check_solution.py
+++ src/check_solution.py
@@ -1,10 +1,17 @@
-# esta función termina el programa si se encuentra la soluión exacta, si no, "intentará" usar como solución el individuo con mejor fitness
+import sys
+
+# esta función termina el programa si se encuentra la solución exacta, si no, intenta usar como solución el individuo con mejor fitness
 def check_solution(solution, new_population, new_fitness):
     max_fitness = max(new_fitness.values())
+
+    # si se encuentra la solución exacta, terminar el programa
     for i, individual in enumerate(new_population):
         if individual == solution:
-            return print(individual, "solution found", solution)
+            print(individual, "solution found", solution)
+            sys.exit(0) # terminar el programa
+
     # si no se encuentra la solución exacta, se intenta usar el individuo con mejor fitness como solución
-        else:
-            if new_fitness[i] == max_fitness:
-                return individual, print(individual, "best attempt used as solution")
+    for i, individual in enumerate(new_population):
+        if new_fitness[i] == max_fitness:
+            print(individual, "best attempt used as solution")
+            return individual
