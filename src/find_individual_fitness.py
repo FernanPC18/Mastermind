442b489388a0591607dce01cb81bde520694a061
LancerFDa <rodrgolorenzo@gmail.com>
2026-01-12 12:52:26 +0100

    refactor(find_individual_fitness): Borrar código inutil

--- src/find_individual_fitness.py
+++ src/find_individual_fitness.py
@@ -9,10 +9,6 @@ def find_individual_fitness(solution, population):
         individual_copy = list(individual)
         solution_copy = list(solution)
 
-        # Iterar sólo hasta la longitud mínima para evitar 'individual out of range'
-        for gene_individual in range(min(len(individual_copy), len(solution_copy))):
-            allele = individual_copy[gene_individual]
-
         for gene_individual, allele_individual in enumerate(individual_copy):
             # Si el alelo ya fue marcado como None (ya procesado), saltarlo
             if allele_individual is None:
@@ -23,13 +19,6 @@ def find_individual_fitness(solution, population):
                 fitness_score += 1
                 individual_copy[gene_individual] = None
                 solution_copy[gene_individual] = None
-
-            # Comprobación de match en otra posición: buscar en solution_copy
-            elif allele_individual in solution_copy:
-                match_index = solution_copy.index(allele_individual)
-                fitness_score += 0
-                individual_copy[gene_individual] = None
-                solution_copy[match_index] = None
 
             # No hay coincidencia
             else:
